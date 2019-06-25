081ec6f3d95bc7233e0c2db9fbb2ac8cf5c4e4b5
dogom <602372120@qq.com>
2019-06-25 10:49:41 +0800

    readme

---
+++ pages/activity/_aid/search/index.vue
@@ -0,0 +1,43 @@
+<template>
+    <!-- 搜索页面 -->
+    <div class="activity_search">
+        <topback />
+
+        <van-search
+                v-model="word"
+                placeholder="请输入商家或学生"
+                show-action
+                @search="onSearch"
+        >
+            <div slot="action" @click="onSearch">搜索</div>
+        </van-search>
+
+        <div>
+
+        </div>
+    </div>
+
+
+</template>
+
+<script>
+    import Topback from "../../../../components/topback";
+    export default {
+        name: "index",
+        data(){
+            return {
+                word:this.$route.query.word
+            }
+        },
+        methods:{
+            onSearch(){
+                console.log(this.word)
+            }
+        },
+        components: {Topback}
+    }
+</script>
+
+<style scoped>
+
+</style>
